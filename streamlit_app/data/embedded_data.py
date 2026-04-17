24c393c38d9168e8b671763967e36a61dacb15c3
jschmoe829-web <jschmoe829@gmail.com>
2026-04-17 16:09:51 -0700

    Update to use 1v1me_Apr17.csv as main data file

--- streamlit_app/data/embedded_data.py
+++ streamlit_app/data/embedded_data.py
@@ -23,6 +23,9 @@ def load_data(csv_path=None):
     
     if csv_path is None:
         possible_paths = [
+            '1v1me_Apr17.csv',
+            os.path.join(os.path.dirname(os.path.dirname(__file__)), '1v1me_Apr17.csv'),
+            'streamlit_app/1v1me_Apr17.csv',
             '1v1me_Apr11.csv',
             os.path.join(os.path.dirname(os.path.dirname(__file__)), '1v1me_Apr11.csv'),
             'streamlit_app/1v1me_Apr11.csv',
